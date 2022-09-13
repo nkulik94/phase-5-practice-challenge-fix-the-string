d795134d7619b1d4e9f49372b08c0b8248f6bf32
Naftali Kulik <nkulik1994@gmail.com>
2022-09-13 17:17:37 -0400

    commit tree 30955d4c315b87e8416ca638ce3b5c0fd3aa5a1b

--- lib/fix_the_string.rb
+++ lib/fix_the_string.rb
@@ -1,1 +1,14 @@
-# your code here
+def fix_the_string string
+    #return string if string == ''
+    chars_stack = []
+    string.chars.map do |char|
+        if chars_stack.empty?
+            chars_stack.push(char)
+        elsif char == chars_stack.last || char.upcase != chars_stack.last.upcase
+            chars_stack.push(char)
+        else
+            chars_stack.pop
+        end
+    end
+    chars_stack.join('')
+end
